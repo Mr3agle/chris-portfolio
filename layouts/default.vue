80ce1ede236b2324cfced9918f8de5420457d9b3
Mr.Eagle <jostinvalencia97@gmail.com>
2020-04-17 16:43:34 -0500

    relayout Home

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,9 +13,7 @@
       <v-list shaped>
         <v-list-item two-line :class="miniVariant && 'px-0'">
           <v-list-item-avatar class="ml-2">
-            <img
-              src="https://lh3.googleusercontent.com/ryDCW2KLJk10pQtH3aRXIBB8aff8OaacPusUn0UxUdrOGvEF1C2blFGd11r0f3wi5Zefk0Ftx1Z2Wcm2nqu_f6loDsosrTMk0n307JmfC-bqcWFNc3H5wAS0W844VqFbO6L8aiS4pqPooy4XujACXvcsPoPBWnx6bTsi4XInKKf0zECuzvX-jlCx9ITSI_BCpIcpb1A3p5vzcCNt9KlJzMY8t-JX2vkKALBJLYhRYLcKIvzjVd-3Br0C15Pss1Sr51EkNdHEB5dCSzqHVqV_NMo7h6fajvDa2gOBF48Qelxz_q3pjkUOVm0T5gDqnEMjTOZ6xrI_VrRpvaki5skmzEPVb9CBLbXMSQq7wJojOhshFwBArg34b_ytbmzRMy38HIYSeVGdb746vYimiT6-l_uU4lHeDbZ76MTDVxCGms26dvw5Zd-LJ_tdGOf4_Jeai72Kg2HKZ_sbXGcn-TiURZhTB4D0h7r93T-s1y6VgTbinftMHD3ovSiBxG7NFaH8fM4i8bgdjqhDVRg3EqA2PZh7uF1y0a2vs9Y36fq-rxLwgRKWntF3jboguv8yMWBLTQwI-rofmMJR7dTldWYNBB0PW-CpNKDA4L1h5QwXMHAwxHDPwTNpxFxzFw_lB2V_PkRwRt0ZV9LgfrDV_4ZAZdf9h6aZ9eo56s3wtC9F8iispfF2Dc7U9G-JA0o5xI8nZQutECEbrnhjDCLpRrkScism19cNld20tS4trLknTtHPB8F4clR7VRk=w1002-h1000-no"
-            />
+            <img src="/img/ChrisQuinnProfilePic.jpeg" />
           </v-list-item-avatar>
 
           <v-list-item-content>
@@ -53,15 +51,19 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
+      <v-btn icon href="https://www.facebook.com/christopher.quinn.5817" target="_blank">
         <v-icon>mdi-facebook</v-icon>
       </v-btn>
 
-      <v-btn icon>
+      <v-btn
+        icon
+        href="https://www.instagram.com/invites/contact/?i=166w0hx1w3v3&utm_content=fcun1p6"
+        target="_blank"
+      >
         <v-icon>mdi-instagram</v-icon>
       </v-btn>
 
-      <v-btn icon>
+      <v-btn icon href="mailto:christopherquinn10@yahoo.com" target="_blank">
         <v-icon>mdi-email</v-icon>
       </v-btn>
     </v-app-bar>
--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,11 @@
     <v-flex xs12 sm8 md6>
       <v-card class="mx-auto my-10" max-width="700px" elevation="0">
         <v-card-text>
-          <div class="text-xs-right text-lg-center">
+          <div class="text-center">
             <img src="@/assets/logo/chq-d.svg" width="400px" />
           </div>
-          <div class="text-xs-right text-lg-center">
-            <img
-              src="https://lh3.googleusercontent.com/ryDCW2KLJk10pQtH3aRXIBB8aff8OaacPusUn0UxUdrOGvEF1C2blFGd11r0f3wi5Zefk0Ftx1Z2Wcm2nqu_f6loDsosrTMk0n307JmfC-bqcWFNc3H5wAS0W844VqFbO6L8aiS4pqPooy4XujACXvcsPoPBWnx6bTsi4XInKKf0zECuzvX-jlCx9ITSI_BCpIcpb1A3p5vzcCNt9KlJzMY8t-JX2vkKALBJLYhRYLcKIvzjVd-3Br0C15Pss1Sr51EkNdHEB5dCSzqHVqV_NMo7h6fajvDa2gOBF48Qelxz_q3pjkUOVm0T5gDqnEMjTOZ6xrI_VrRpvaki5skmzEPVb9CBLbXMSQq7wJojOhshFwBArg34b_ytbmzRMy38HIYSeVGdb746vYimiT6-l_uU4lHeDbZ76MTDVxCGms26dvw5Zd-LJ_tdGOf4_Jeai72Kg2HKZ_sbXGcn-TiURZhTB4D0h7r93T-s1y6VgTbinftMHD3ovSiBxG7NFaH8fM4i8bgdjqhDVRg3EqA2PZh7uF1y0a2vs9Y36fq-rxLwgRKWntF3jboguv8yMWBLTQwI-rofmMJR7dTldWYNBB0PW-CpNKDA4L1h5QwXMHAwxHDPwTNpxFxzFw_lB2V_PkRwRt0ZV9LgfrDV_4ZAZdf9h6aZ9eo56s3wtC9F8iispfF2Dc7U9G-JA0o5xI8nZQutECEbrnhjDCLpRrkScism19cNld20tS4trLknTtHPB8F4clR7VRk=w1002-h1000-no"
-              width="300px"
-              style="border-radius: 50%"
-            />
+          <div class="text-center">
+            <img src="/img/ChrisQuinnProfilePic.jpeg" width="300px" style="border-radius: 50%" />
           </div>
 
           <p class="text-justify mt-5">
@@ -23,33 +19,50 @@
 
           <hr class="my-5" />
 
-          <div class="section-title text-center">Contacto</div>
-          <div class="my-3 text-center">
-            <v-btn class text x-large color="blue" href="tel:+593 98 568 6956">
-              <v-icon>mdi-phone</v-icon>&nbsp; 098 568 6956
-            </v-btn>
-          </div>
-          <div class="my-3 text-center">
-            <v-btn class text x-large color="blue" href="mailto: christopherquinn@yahoo.com">
-              <v-icon>mdi-email</v-icon>&nbsp; christopherquinn@yahoo.com
-            </v-btn>
-          </div>
+          <v-row>
+            <v-col>
+              <div class="section-title-home text-center">Contacto</div>
+              <v-row justify="center" class="mt-5">
+                <v-btn class text x-large href="tel:+593 98 568 6956">
+                  <v-icon large color="black lighten-5" class="mx-3">mdi-phone</v-icon>
+                </v-btn>
 
-          <div class="section-title text-center">Redes Sociales</div>
-          <div class="my-3">
-            <v-row justify="center">
-              <v-btn class text x-large color="blue" href="#">
-                <v-icon large color="blue" class="mx-3">mdi-facebook</v-icon>
-              </v-btn>
-              <v-btn class text x-large color="pink" href="#">
-                <v-icon large color="pink lighten-3" class="mx-3">mdi-instagram</v-icon>
-              </v-btn>
-              <v-btn class text x-large color="teal" href="#">
-                <v-icon large color="teal" class="mx-3">mdi-email</v-icon>
-              </v-btn>
-            </v-row>
-          </div>
-          <div class="section-title text-center">Logos de Marcas</div>
+                <v-btn class text x-large href="mailto: christopherquinn10@yahoo.com">
+                  <v-icon large color="black lighten-5" class="mx-3">mdi-email</v-icon>
+                </v-btn>
+              </v-row>
+            </v-col>
+            <v-col>
+              <div class="section-title-home text-center">Redes Sociales</div>
+              <v-row justify="center" class="mt-5">
+                <v-btn
+                  class
+                  text
+                  x-large
+                  color="blue"
+                  href="https://www.facebook.com/christopher.quinn.5817"
+                  target="_blank"
+                >
+                  <v-icon large color="blue" class="mx-3">mdi-facebook</v-icon>
+                </v-btn>
+                <v-btn
+                  class
+                  text
+                  x-large
+                  color="pink"
+                  href="https://www.instagram.com/invites/contact/?i=166w0hx1w3v3&utm_content=fcun1p6"
+                  target="_blank"
+                >
+                  <v-icon large color="pink lighten-3" class="mx-3">mdi-instagram</v-icon>
+                </v-btn>
+                <v-btn class text x-large color="teal" href="mailto:christopherquinn10@yahoo.com">
+                  <v-icon large color="teal" class="mx-3">mdi-email</v-icon>
+                </v-btn>
+              </v-row>
+            </v-col>
+          </v-row>
+          <div class="my-3"></div>
+          <div class="section-title text-center">Logos de Clientes</div>
 
           <v-row justify="center mt-10">
             <img src="img/logoPI.jpg" alt="logopi" width="250px" class="mx-4" />
